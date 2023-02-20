dd2b4a61e830138a096340fb6fea1de3e9debd01
Int Fract <intfract@gmail.com>
2023-02-20 02:10:15 +0000

    created app

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+import { render } from 'inflict'
+import express from 'express'
+const app = express()
 
+app.use(express.static('public'))
+
+app.get('/', async (req, res) => {
+  res.send(render('views', '', { message: 'Hello!', todo: ['Eat', 'Code'] })) 
+})
+
+app.listen(3000)
